b7602052602e37c537df0c012043b11ba8b175b5
Giuseppe Nicotra <148492201+pepni@users.noreply.github.com>
2023-11-01 17:47:43 +0100

    Add files via upload
    01-11-2023

---
+++ 01-11-23/script.js
@@ -0,0 +1,14 @@
+//const animalNames = [
+"seagull",
+  "monkey",
+  "rabbit",
+  "mouse",
+  "firefly",
+  "edgehog",
+  "bear",
+  "fox",
+  //];
+  //for (let i = 0; i < animalNames.length; i++) {
+  //  console.log(i + ":" + animalNames[i]);
+  //}
+  console.log(2);
